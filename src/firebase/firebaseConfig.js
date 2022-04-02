0fe020b918b2c17fec0c0136ba5c719e3d382f08
Hugo Wride <hugowride@gmail.com>
2022-04-02 21:30:05 +0100

    Add basic Firebase integration.

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+  apiKey: 'AIzaSyAp1nCullOMFN6rnARX4Xrjs05O73jQ8JI',
+  authDomain: 'to-do-20da5.firebaseapp.com',
+  projectId: 'to-do-20da5',
+  storageBucket: 'to-do-20da5.appspot.com',
+  messagingSenderId: '173435279385',
+  appId: '1:173435279385:web:fc74fee31c48f74464f288',
+  measurementId: 'G-Q41RC5C14D',
+};
