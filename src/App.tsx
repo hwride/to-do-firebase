235892d84c49137d1bd5e15f68818409535ed9f6
Hugo Wride <hugowride@gmail.com>
2022-04-02 20:38:49 +0100

    Add prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      To do
-    </div>
-  );
+  return <div className="App">To do</div>;
 }
 
 export default App;
