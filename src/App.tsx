a93d274b74b745501be3b00f3c83029d95e4bbfb
Hugo Wride <hugowride@gmail.com>
2022-12-19 22:34:41 +0000

    Get multiple different translations working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IntlProvider } from 'react-intl';
 import './App.css';
 import { firebaseAuthButtonStyleOverrides } from './auth/SignInScreen';
 import { useUserState } from './auth/useUserState';
 import ToDoList from './components/ToDoList/ToDoList';
 import './firebase/firebase';
-import { enGB } from './lang/en-GB';
+
+type SupportedLocales = 'en' | 'fr' | 'ru';
 
 function App() {
   const userState = useUserState();
+  const [localeChoice, setLocaleChoice] = useState<SupportedLocales>('ru');
+  const [localeLoaded, setLocaleLoaded] = useState<{
+    locale: SupportedLocales;
+    messages: Record<string, string>;
+  }>();
+  useEffect(() => {
+    (async function () {
+      const messages = await loadLocaleData(localeChoice);
+      setLocaleLoaded({
+        locale: localeChoice,
+        messages,
+      });
+    })();
+  }, [localeChoice]);
 
-  return (
-    <IntlProvider locale={navigator.language} messages={enGB}>
+  return localeLoaded ? (
+    <IntlProvider locale={localeLoaded.locale} messages={localeLoaded.messages}>
       <div className="App">
         <style>{firebaseAuthButtonStyleOverrides}</style>
         <ToDoList userState={userState} />
       </div>
     </IntlProvider>
-  );
+  ) : null;
+}
+
+async function loadLocaleData(locale: string) {
+  const localeFinal = ['en', 'fr', 'ru'].includes(locale) ? locale : 'en';
+  const localeModule = await import(`./lang/${localeFinal}.json`);
+  const messages = localeModule.default;
+  return messages;
 }
 
 export default App;
--- src/lang/en-GB.ts
+++
@@ -1,9 +0,0 @@
-export const enGB = {
-  appTitle: 'To do list',
-  signInToViewText: 'Sign in to view to do items.',
-  signOutButton: 'Sign-out',
-  addToDoButton: 'Add to do',
-  themeButton: 'Theme',
-  themeLight: 'Light',
-  themeDark: 'Dark',
-};
